331403e7d30afb588c5f6d6c0ff95f98c2aef70f
Olmo Zavala Romero <olmozavala@gmail.com>
2017-11-06 18:36:50 -0600

    Fixed bug un RunGOESSerer.sh

--- RunGOESServer.sh
+++ RunGOESServer.sh
@@ -3,5 +3,4 @@
 tiffOutFolder='/ServerData/GOES/tiffs/'
 jpgOutFolder='/ServerData/GOES/images/'
 fileType='mextiff'
-python /home/olmozavala/Dropbox/MyProjects/GOES/Animations/Main.py $tiffOutFolder $jpgOutFolder $fileType
-/usr/local/anaconda/bin/python /ServerScripts/GOES/Main.py $tiffOutFolder $jpgOutFolder
+/usr/local/anaconda/bin/python /ServerScripts/GOES/Main.py $tiffOutFolder $jpgOutFolder $fileType
