77c9f50730e12c56866bb83b9c88b74ee7d3491d
ixchel <ixchelzg@gmail.com>
2018-01-19 16:50:34 -0600

    queries.php returns only images of todays date.

--- animation/queries.php
+++ animation/queries.php
@@ -2,13 +2,16 @@
   session_start();
 
   $nFile=scandir("images/");
-  $ImageNom="Mexico_2017.0906.";
-  //$lastDirectory=$nFile[2];
-  //print($lastDirectory);
-     
+  $ImageNom="Mexico_2018.".date("m");
+  //print($ImageNom);
   $ruta_completa = "images/".$ImageNom ;
   $row_imagenes[0] = count($nFile)-2;
 
   $directory = "images/";
-  $images = glob($directory . "*.jpg");
+  $imagestmp = glob($directory . "*.jpg");
+  //print_r($imagestmp);
+  $images = preg_grep("~Mexico_2018.".date("m").date("d")."~", $imagestmp);
+  $images = array_values($images);
+  $row_imagenes[0] = count($images)-1;
+  //print_r($images);
 ?>
